aca201ee251c25fb6b907d1e0efafca1e7f7e2c2
senaboyuktas <senabykts99@gmail.com>
2024-02-08 20:19:50 +0300

    84. ders sonu

---
+++ MediatorDesignPattern/DesignPattern.Mediator/DAL/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace DesignPattern.Mediator.DAL
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=(localdb)\\MSSQLLocalDB; initial catalog=DesignPattern8; integrated security=true;");
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
